11f447ce87edd5cc41e32bc57556a19bc3722f17
Nick Loadholtes <nick@ironboundsoftware.com>
2012-07-27 11:08:57 -0400

    Splitting the output files into different places

--- n-body/run_py3_test.sh
+++ n-body/run_py3_test.sh
@@ -1,2 +1,2 @@
 echo `date` >> timings.log
-/usr/bin/time --format=" %U sec" -a -o timings.log python3 nbody.py 50000 >> timings.log
+/usr/bin/time --format=" %U sec" -a -o timings.log python3 nbody.py 50000 >> timings_py3.log
--- n-body/run_py_test.sh
+++ n-body/run_py_test.sh
@@ -1,2 +1,2 @@
 echo `date` >> timings.log
-/usr/bin/time --format=" %U sec" -a -o timings.log python3 nbody.py 50000 >> timings.log
+/usr/bin/time --format=" %U sec" -a -o timings.log python nbody.py 50000 >> timings_py.log
